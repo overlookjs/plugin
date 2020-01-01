eaeb6467f47d2d678f595c68753a671b6324f907
overlookmotel <theoverlookmotel@gmail.com>
2020-01-01 20:03:35 +0000

    Initial release

--- README.md
+++ README.md
@@ -9,9 +9,267 @@
 
 Part of the [Overlook framework](https://overlookjs.github.io/).
 
+## Introduction
+
+[Overlook framework](https://overlookjs.github.io/) `Router` class. Route class extensions (otherwise known as "Routers") should be created with this class.
+
+Overlook is intended to be extremely modular and flexible.
+
+The base `Route` class has very little functionality, and most functionality is intended to be added using Routers.
+
+Extensions (or "Routers") are much like "plugins", which other frameworks use, but the main difference is this:
+
+> Route class extensions apply at the route level, not application level.
+
+One route, or subtree of routes, can have one behavior, another subtree can have another. So, for example, one part of the app can use [React](https://reactjs.org/), another part can server-render pages from [EJS](https://ejs.co/) templates.
+
+This architecture allows:
+
+1. "Snap in" extensions providing common functionality, making building apps fast
+2. Granular control over every route's individual behavior
+
 ## Usage
 
-This module is under development and not ready for use yet.
+### Creating a Router
+
+A Router is created from a function which receives a `Route` class and should return a subclass of it.
+
+```js
+const Router = require('@overlook/router'),
+  makeSymbols = require('@overlook/util-make-symbols');
+
+const { TYPE, GREETING } = makeSymbols(
+  [ 'TYPE', 'GREETING' ]
+);
+
+const mammalRouter = new Router( Route => (
+  class extends Route {
+    initProps( props ) {
+      super.initProps( props );
+      this[TYPE] = undefined;
+    }
+
+    initRoute( app ) {
+      super.initRoute( app );
+      if (this[TYPE] === undefined) this[TYPE] = 'mammal';
+    }
+
+    [GREETING]() {
+      return `Hello, I am a ${this[TYPE]}.`;
+    }
+  }
+) );
+
+mammalRouter.TYPE = TYPE;
+mammalRouter.GREETING = GREETING;
+```
+
+New methods and properties should have Symbol keys, not strings. If properties are intended to be accessed by other Routers, or methods intended to be available for extending, the Symbol should be exported as a property of the Router.
+
+The Router can than be used create a subclass of a `Route` class, using `.extend()`:
+
+```js
+const Route = require('@overlook/route');
+
+const MammalRoute = Route.extend( mammalRouter );
+```
+
+That subclass can now be used to create a route which includes the Router's functionality:
+
+```js
+const route = new MammalRoute();
+
+route[ mammalRouter.GREETING ]()
+// => 'Hello, I am a mammal.'
+```
+
+### Removing Symbols boilerplate
+
+It's typical for a Router to define Symbols.
+
+To reduce boilerplate code, you can create Symbols within the Router constructor.
+
+This example is equivalent to the first:
+
+```js
+const Router = require('@overlook/router');
+
+const mammalRouter = new Router( {
+  symbols: [ 'TYPE', 'GREETING' ],
+  extend
+} );
+
+const { TYPE, GREETING } = mammalRouter;
+
+function extend( Route ) (
+  class extends Route {
+    initProps( props ) {
+      super.initProps( props );
+      this[TYPE] = undefined;
+    }
+
+    initRoute( app ) {
+      super.initRoute( app );
+      if (this[TYPE] === undefined) this[TYPE] = 'mammal';
+    }
+
+    [GREETING]() {
+      return `Hello, I am a ${this[TYPE]}.`;
+    }
+  }
+}
+```
+
+Note that the symbols were set as properties of `mammalRouter`.
+
+### Publishing a Router to NPM
+
+`new Router()` has a few more options which should be used when publishing a Router to NPM.
+
+1. You should pass in the name and version of the module.
+2. Symbols *must* be created using either `@overlook/util-make-symbols` or the `Router` constructor.
+
+```js
+// Published as `@me/monkey`
+// version 1.0.0
+const Router = require('@overlook/router'),
+  makeSymbols = require('@overlook/util-make-symbols');
+
+const { GREETING } = makeSymbols(
+  '@me/monkey',
+  [ 'GREETING' ]
+);
+
+const monkeyRouter = new Router(
+  {
+    name: '@me/monkey',
+    version: '1.0.0'
+  },
+  Route => class extends Route { /* ... */ }
+);
+
+module.exports = monkeyRouter;
+```
+
+The options object has the same properties as `package.json` so, to avoid having to update the version property every time you publish a new version of the module, you can pass that instead.
+
+It's also more economical to create symbols in `new Router()`, to avoid passing the package name to `makeSymbols()` too:
+
+```js
+const pkg = require('./package.json');
+
+const monkeyRouter = new Router(
+  pkg,
+  { symbols: [ 'GREETING' ] },
+  Route => class extends Route { /* ... */ }
+);
+
+module.exports = monkeyRouter;
+```
+
+### Longer example including using symbols
+
+```js
+const Router = require('@overlook/router');
+const pkg = require('./package.json');
+
+const monkeyRouter = new Router(
+  pkg,
+  { symbols: [ 'GREETING' ] },
+  extend
+);
+
+const { GREETING } = monkeyRouter;
+
+function extend( Route ) {
+  return class extends Route {
+    [GREETING]() {
+      return 'Hello, I am a monkey.';
+    }
+  };
+}
+
+module.exports = monkeyRouter;
+```
+
+### Composing Routers
+
+Routers can extend other Routers.
+
+Let's say we have various routes which need a "greeting" method. This functionality can be split off into its own Router.
+
+```js
+const Router = require('@overlook/router');
+
+const typeRouter = new Router(
+  { symbols: [ 'TYPE', 'GET_TYPE', 'GREETING' ] },
+  extend
+} );
+
+const { TYPE, GET_TYPE, GREETING } = typeRouter;
+
+function extend( Route ) (
+  class extends Route {
+    initProps( props ) {
+      super.initProps( props );
+      this[TYPE] = undefined;
+    }
+
+    initRoute( app ) {
+      super.initRoute( app );
+      this[TYPE] = this[GET_TYPE]();
+    }
+
+    [GET_TYPE]() {
+      return 'mystery';
+    }
+
+    [GREETING]() {
+      return `Hello, I am a ${this[TYPE]}.`;
+    }
+  }
+}
+```
+
+Now other Routers can extend off that:
+
+```js
+const { GET_TYPE } = typeRouter;
+
+const monkeyRouter = new Router(
+  Route => {
+    Route = Route.extend( typeRouter );
+
+    return class extends Route {
+      [GET_TYPE]() {
+        return 'monkey';
+	  }
+    };
+  }
+);
+
+const zebraRouter = new Router(
+  Route => {
+    Route = Route.extend( typeRouter );
+
+    return class extends Route {
+      [GET_TYPE]() {
+        return 'zebra';
+	  }
+    };
+  }
+);
+
+const MonkeyRoute = Route.extend( monkeyRouter );
+const monkey = new MonkeyRoute();
+monkey[GREETNG]()
+// => 'Hello, I am a monkey.'
+
+const ZebraRoute = Route.extend( zebraRouter );
+const zebra = new ZebraRoute();
+zebra[GREETNG]()
+// => 'Hello, I am a zebra.'
+```
 
 ## Tests
 
--- test/index.test.js
+++ test/index.test.js
@@ -6,15 +6,249 @@
 'use strict';
 
 // Modules
-const Router = require('../index');
-
-// Init
-require('./support');
+const store = require('@overlook/symbol-store'),
+	{isArray} = require('is-it-type'),
+	flatMap = require('core-js-pure/features/array/flat-map'),
+	Router = require('../index');
 
 // Tests
 
-describe('tests', () => {
-	it.skip('all', () => { // eslint-disable-line jest/no-disabled-tests
-		expect(Router).not.toBeUndefined();
+// Clear store before each test
+beforeEach(() => {
+	for (const key in store) {
+		delete store[key];
+	}
+
+	expect(store).toContainAllKeys([]); // eslint-disable-line jest/no-standalone-expect
+});
+
+it('exports a class', () => {
+	expect(Router).toBeFunction();
+});
+
+it('creates a Router instance', () => {
+	const router = new Router(() => {});
+	expect(router).toBeInstanceOf(Router);
+});
+
+describe('records arguments', () => {
+	const argsStrs = makeArgStrings(['name', 'version', 'extend']);
+
+	// Sanity check - check tests are covering all possibilities
+	it('tests cover all possibilities', () => {
+		expect(argsStrs).toHaveLength(28);
+	});
+
+	for (const argsStr of argsStrs) {
+		describe(argsStr, () => {
+			let router, extend;
+			beforeEach(() => {
+				extend = () => {};
+				const createArgs = makeCreateArgsFn(argsStr);
+				const args = createArgs('foo', '1.0.0', extend);
+				router = new Router(...args);
+			});
+
+			it('records name', () => {
+				expect(router.name).toBe('foo');
+			});
+
+			it('records version', () => {
+				expect(router.version).toBe('1.0.0');
+			});
+
+			it('records extend function', () => {
+				expect(router.extend).toBe(extend);
+			});
+		});
+	}
+});
+
+describe('creates symbols', () => {
+	describe('with no name, when arguments', () => {
+		const argsStrs = makeArgStrings(['symbols', 'extend']);
+
+		// Sanity check - check tests are covering all possibilities
+		it('tests cover all possibilities', () => {
+			expect(argsStrs).toHaveLength(5);
+		});
+
+		for (const argsStr of argsStrs) {
+			describe(argsStr, () => {
+				let createArgs;
+				beforeEach(() => {
+					const createArgsFull = makeCreateArgsFn(argsStr);
+					createArgs = symbols => createArgsFull(null, null, () => {}, symbols);
+				});
+
+				it('returns symbols', () => {
+					const args = createArgs(['BAR', 'QUX']);
+					const {BAR, QUX} = new Router(...args);
+					expect(typeof BAR).toBe('symbol');
+					expect(String(BAR)).toBe('Symbol(BAR)');
+					expect(typeof QUX).toBe('symbol');
+					expect(String(QUX)).toBe('Symbol(QUX)');
+				});
+
+				it('does not cache symbols', () => {
+					const args1 = createArgs(['BAR']);
+					const router1 = new Router(...args1);
+
+					const args2 = createArgs(['BAR']);
+					const router2 = new Router(...args2);
+
+					expect(typeof router1.BAR).toBe('symbol');
+					expect(typeof router2.BAR).toBe('symbol');
+					expect(router1.BAR).not.toBe(router2.BAR);
+				});
+			});
+		}
+	});
+
+	describe('with name, when arguments', () => {
+		const argsStrs = makeArgStrings(['name', 'version', 'symbols', 'extend']);
+
+		// Sanity check - check tests are covering all possibilities
+		it('tests cover all possibilities', () => {
+			expect(argsStrs).toHaveLength(145);
+		});
+
+		for (const argsStr of argsStrs) {
+			describe(argsStr, () => {
+				let createArgs;
+				beforeEach(() => {
+					const createArgsFull = makeCreateArgsFn(argsStr);
+					createArgs = symbols => createArgsFull('foo', '1.0.0', () => {}, symbols);
+				});
+
+				it('returns symbols', () => {
+					const args = createArgs(['BAR', 'QUX']);
+					const {BAR, QUX} = new Router(...args);
+					expect(typeof BAR).toBe('symbol');
+					expect(String(BAR)).toBe('Symbol(foo.BAR)');
+					expect(typeof QUX).toBe('symbol');
+					expect(String(QUX)).toBe('Symbol(foo.QUX)');
+				});
+
+				it('caches symbols', () => {
+					const args1 = createArgs(['BAR']);
+					const router1 = new Router(...args1);
+
+					const args2 = createArgs(['BAR']);
+					const router2 = new Router(...args2);
+
+					expect(typeof router1.BAR).toBe('symbol');
+					expect(typeof router2.BAR).toBe('symbol');
+					expect(router1.BAR).toBe(router2.BAR);
+				});
+			});
+		}
 	});
 });
+
+function makeCreateArgsFn(argsStr) {
+	// eslint-disable-next-line no-eval
+	return eval(`(name, version, extend, symbols) => [${argsStr}]`);
+}
+
+function makeArgStrings(names) {
+	// Make all possible orders of arg names
+	let iterations = [[]];
+	for (let i = 0; i < names.length; i++) {
+		iterations = flatMap(iterations, args => (
+			names.filter(name => !args.find(arg => arg.name === name))
+				.map(name => args.concat([{name}]))
+		));
+	}
+
+	// Add all possible combinations of arg names
+	const ARG = 0,
+		PROP = 1,
+		PROP_JOINED = 2;
+	const ARG_TYPES = [ARG, PROP, PROP_JOINED];
+
+	for (let i = 0; i < names.length; i++) {
+		iterations = flatMap(iterations, args => (
+			ARG_TYPES.map((type) => {
+				const listCloned = args.map(arg => ({...arg}));
+				listCloned[i].type = type;
+				return listCloned;
+			})
+		));
+	}
+
+	// Filter out invalid combinations
+	const prioritiesMap = {};
+	names.forEach((arg, index) => { prioritiesMap[arg] = index; });
+
+	iterations = iterations.filter((args) => {
+		// PROP_JOINED type cannot be first or follow ARG
+		let lastType = ARG;
+		for (const {type} of args) {
+			if (type === PROP_JOINED && lastType === ARG) return false;
+			lastType = type;
+		}
+
+		// `name` can only be string if is first arg
+		// `version` can only be string if is 2nd arg and after `name` as string
+		if (names.includes('name')) {
+			const namePos = args.findIndex(arg => arg.name === 'name'),
+				nameType = args[namePos].type;
+			if (nameType === ARG && namePos !== 0) return false;
+
+			const versionPos = args.findIndex(arg => arg.name === 'version'),
+				versionType = args[versionPos].type;
+			if (versionType === ARG && (versionPos !== 1 || nameType !== ARG)) return false;
+		}
+
+		// `symbols` can only be an object prop
+		if (names.includes('symbols')) {
+			const symbolsType = args.find(arg => arg.name === 'symbols').type;
+			if (symbolsType === ARG) return false;
+		}
+
+		// Eliminate objects where props not in priority order
+		// i.e. Don't need to test both `{name, version}` and `{version, name}`
+		let lastPriority = 0;
+		for (const arg of args) {
+			const {type} = arg;
+			if (type === ARG) {
+				lastPriority = 0;
+				continue;
+			}
+
+			const priority = prioritiesMap[arg.name];
+			if (arg.type === PROP_JOINED && priority < lastPriority) return false;
+
+			lastPriority = priority;
+		}
+
+		return true;
+	});
+
+	// Convert to argument strings
+	return iterations.map((args) => {
+		// Assemble argument string
+		let lastPart = null;
+		const parts = flatMap(args, ({name, type}) => {
+			if (type === ARG) {
+				lastPart = null;
+				return [name];
+			}
+
+			if (type === PROP || !lastPart) {
+				const part = [name];
+				lastPart = part;
+				return [part];
+			}
+
+			lastPart.push(name);
+			return [];
+		});
+
+		return parts.map((part) => {
+			if (!isArray(part)) return part;
+			return `{${part.join(', ')}}`;
+		}).join(', ');
+	});
+}
